fb925d53c3a5de82600ed71723827f92acc02843
Cactiw <Knyazev1514@gmail.com>
2024-01-14 21:32:35 +0300

    Translations fix

--- fastapi_user_auth/globals/deps.py
+++ fastapi_user_auth/globals/deps.py
@@ -5,6 +5,8 @@ from fastapi import Depends
 from fastapi_user_auth import globals as g
 from fastapi_user_auth.auth.exceptions import AuthError, ErrorCode
 
+from fastapi_amis_admin.utils.translation import i18n as _
+
 # 获取当前登录的用户
 get_user_or_none = g.auth.get_current_user
 
@@ -14,7 +16,7 @@ CurrentUserOrNone: Optional[g.UserModel] = Annotated[Optional[g.UserModel], Depe
 def get_user_or_error(user: CurrentUserOrNone):
     """获取当前登录用户,如果未登录则抛出异常"""
     if not user:
-        raise AuthError(status=ErrorCode.USER_IS_NOT_LOGIN, msg="用户未登录")
+        raise AuthError(status=ErrorCode.USER_IS_NOT_LOGIN, msg=_("User not logged in"))
     return user
 
 
